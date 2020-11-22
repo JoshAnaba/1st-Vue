89e57a1727638e68e3d8873df5436192f798e69e
Josh <anabajoshchi@gmail.com@>
2020-11-22 18:09:34 +0100

    progress

--- main.js
+++ main.js
@@ -3,39 +3,57 @@ var app = new Vue({
     data: {
         product : 'Socks' ,
         color: 'blue',
-        image: 'img/green-sock.png' ,
+        selectedVariant: 0,
         link: 'vue2.html' ,
-        onSale : true ,
+        brand: 'Vue Mastery',
         details : ['80% cotton' , '20% polyester' , 'Gender-neutral'],
         variants : [
             {
                 variantId : 100,
                 variantColor: 'blue',
-                variantImage: 'img/blue-sock.png'
+                variantImage: 'img/blue-sock.png',
+                variantQty : 0,
             },
             {
                 variantId : 101,
                 variantColor: 'green',
-                variantImage: 'img/green-sock.png'
+                variantImage: 'img/green-sock.png',
+                variantQty: 100,
             }
             
         ],
 
         sizes: ['small','Large', 'XL' , 'XXL'],
         cart : 0,
+        checkBackLater: {
+            textDecoration: 'line-through',
+        }
     },
     methods: {
         addToCart : function() {
               this.cart ++
         },
 
-        removeFromCart : function(){
+        removeFromCart (){
             if(this.cart !== 0){
                 this.cart --
             }
         },
-        updateProductImage: function (variantImage) {
-            this.image = variantImage
+        updateProductImage(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        onSale(){
+            return this.variants[this.selectedVariant].variantQty
         }
     }
 })
+
